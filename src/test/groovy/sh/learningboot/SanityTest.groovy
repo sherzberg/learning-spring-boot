5e63e07ddcb0ae8cd395674e2f2e7e877b3757b7
Spencer Herzberg <spencer.herzberg@gmail.com>
2014-02-03 19:47:25 -0600

    initial commit; simple spring boot app with spock groovy tests

---
+++ src/test/groovy/sh/learningboot/SanityTest.groovy
@@ -0,0 +1,12 @@
+package sh.learningboot
+
+import spock.lang.Specification
+
+class SanityTest extends Specification {
+
+    void "testing spock works"(){
+        expect:
+        true
+    }
+
+}
